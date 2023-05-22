28fe7af0d7f768b8bc9a2674701520b25d8a85ab
BSSSATHVIK <sathvik7@gmail.com>
2023-05-22 15:37:30 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Is used to bulid the ML application as a package
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+        This function will return the list of requiremnrts
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sathvik',
+    author_email='sathvik7@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
